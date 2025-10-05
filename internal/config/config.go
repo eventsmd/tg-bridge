523524230f7445e9a18063018756b9b9ec5490f2
veet <vitaliy@velikodniy.name>
2025-10-05 12:04:39 +0100

    telegram channel fetch #9 (#16)

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,12 +4,18 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
+	"tg-bridge/internal/domain"
+	"tg-bridge/internal/tgclient"
 )
 
 type Config struct {
 	PostgresConnectionString string
 	TelegramApiId            int
 	TelegramApiHash          string
+	TelegramChannels         map[domain.Supplier]string
+	TelegramChannelsSession  map[domain.Supplier]tgclient.Channel
+	TelegramFetchInterval    int
 	TelegramSession          string
 	TemporalHostPort         string
 	TemporalNamespace        string
@@ -28,6 +34,9 @@ func CheckConfigFields(config Config) {
 	if config.PostgresConnectionString == "" ||
 		config.TelegramApiId == 0 ||
 		config.TelegramApiHash == "" ||
+		config.TelegramChannels == nil ||
+		len(config.TelegramChannels) == 0 ||
+		config.TelegramFetchInterval == 0 ||
 		config.TelegramSession == "" ||
 		config.TemporalHostPort == "" ||
 		config.TemporalNamespace == "" ||
@@ -43,10 +52,16 @@ func InitConfig() Config {
 	if port == 0 {
 		port = 8080
 	}
+	telegramFetchInterval, _ := strconv.Atoi(os.Getenv("TELEGRAM_FETCH_INTERVAL"))
+	if telegramFetchInterval == 0 {
+		telegramFetchInterval = 60
+	}
 	config := Config{
 		PostgresConnectionString: os.Getenv("POSTGRES_CONNECTION_STRING"),
 		TelegramApiId:            telegramApiId,
 		TelegramApiHash:          os.Getenv("TELEGRAM_API_HASH"),
+		TelegramChannels:         parseChannel(os.Getenv("TELEGRAM_CHANNELS")),
+		TelegramFetchInterval:    telegramFetchInterval,
 		TelegramSession:          os.Getenv("TELEGRAM_SESSION"),
 		TemporalHostPort:         os.Getenv("TEMPORAL_HOST_PORT"),
 		TemporalNamespace:        os.Getenv("TEMPORAL_NAMESPACE"),
@@ -56,4 +71,30 @@ func InitConfig() Config {
 	}
 
 	return config
+}
+
+func parseChannel(channel string) map[domain.Supplier]string {
+	result := make(map[domain.Supplier]string)
+	if channel == "" {
+		return result
+	}
+
+	parts := strings.Split(channel, ",")
+	for _, p := range parts {
+		p = strings.TrimSpace(p)
+		if p == "" {
+			continue
+		}
+		kv := strings.SplitN(p, "=", 2)
+		if len(kv) != 2 {
+			continue
+		}
+		k := strings.TrimSpace(kv[0])
+		v := strings.TrimSpace(kv[1])
+		if k == "" || v == "" {
+			continue
+		}
+		result[domain.Supplier{Type: k}] = v
+	}
+	return result
 }
